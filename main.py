3fe2ecfdbe118b5f339c6f79878017de7552f748
rahulpr7 <iamrahulprasad7@gmail.com>
2026-01-16 20:49:27 +0530

    Training Pipeline Ready

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from CNN_Classifier import logger
 from CNN_Classifier.pipeline.Stage1_Data_Ingestion import DataIngestionTrainingPipeline
+from CNN_Classifier.pipeline.Stage2_Prepare_Base import PrepareBaseModelTrainingPipeline
+from CNN_Classifier.pipeline.Stage3_Training import ModelTrainingPipeline
 
 
 
@@ -13,3 +15,30 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare base model"
+
+try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Training"
+
+try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/CNN_Classifier/pipeline/Stage3_Training.py
@@ -0,0 +1,20 @@
+from CNN_Classifier.config.configuration import ConfigurationManager
+from CNN_Classifier.components.Train_Model import Training
+from CNN_Classifier import logger
+
+
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+
+        
