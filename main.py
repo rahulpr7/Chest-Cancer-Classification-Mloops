7d29560dd9447a030a002fa981309467598a21f5
rahulpr7 <iamrahulprasad7@gmail.com>
2026-01-16 19:58:45 +0530

    Data Ingestion Pipeline

---
+++ main.py
@@ -0,0 +1,15 @@
+from CNN_Classifier import logger
+from CNN_Classifier.pipeline.Stage1_Data_Ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
